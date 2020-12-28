986d1446404a7ba8c5abdcb52101f561b3c45f15
ahsanrifat <frifat2013@gmail.com>
2020-12-28 17:04:56 +0600

    Made ingredients a part of recipe and showed them on recipe details

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipes.model';
 
 @Injectable({
@@ -6,10 +7,18 @@ import { Recipe } from './recipes.model';
 })
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe("recipe 01", "test recipe", "https://hips.hearstapps.com/delish/assets/17/39/1506456246-delish-healthy-chicken-casserole-1.jpg"),
-    new Recipe("recipe 02", "test recipe", "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-taco-lime-shrimp-still006-1527253576.jpeg"),
-    new Recipe("recipe 03", "test recipe", "https://cdn.loveandlemons.com/wp-content/uploads/2020/03/baking-recipes-1.jpg"),
-    new Recipe("recipe 04", "test recipe", "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2019%2F11%2Fcranberry-salad.jpg"),
+    new Recipe("recipe 01", "test recipe",
+      "https://hips.hearstapps.com/delish/assets/17/39/1506456246-delish-healthy-chicken-casserole-1.jpg",
+      [new Ingredient("Sauce", 5), new Ingredient("Fries", 10), new Ingredient("Pasta", 2)]),
+    new Recipe("recipe 02", "test recipe",
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-taco-lime-shrimp-still006-1527253576.jpeg",
+      [new Ingredient("Sauce", 5), new Ingredient("Meat", 5), new Ingredient("Wheat", 5)]),
+    new Recipe("recipe 03", "test recipe",
+      "https://cdn.loveandlemons.com/wp-content/uploads/2020/03/baking-recipes-1.jpg",
+      [new Ingredient("Flour", 5), new Ingredient("Chilli", 5), new Ingredient("Onion", 5)]),
+    new Recipe("recipe 04", "test recipe",
+      "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2019%2F11%2Fcranberry-salad.jpg",
+      [new Ingredient("Garlic", 5), new Ingredient("Sauce", 5), new Ingredient("Egg Plant", 5)]),
   ];
   constructor() { }
   getRecipes() {
--- src/app/recipes/recipes.model.ts
+++ src/app/recipes/recipes.model.ts
@@ -1,13 +1,9 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 // we define how a single recipe looks like
 export class Recipe {
-  public name: string;
-  public description: string;
-  public imagePath: string;
 
-  constructor(name: string, description: string, imagePath: string) {
-    this.name = name;
-    this.description = description;
-    this.imagePath = imagePath;
+  constructor(public name: string, public description: string, public imagePath: string, public ingredients: Ingredient[]) {
   }
 }
 

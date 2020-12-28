3efbc68168d7e0d621b044c5e7ec06ac5ca67714
ahsanrifat <frifat2013@gmail.com>
2020-12-29 00:07:54 +0600

    Added ad ingredients from selected recipe functionality

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,6 @@
+
 import { Component, Input, OnInit } from '@angular/core';
+import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
 import { Recipe } from '../recipes.model';
 
 @Component({
@@ -8,9 +10,15 @@ import { Recipe } from '../recipes.model';
 })
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe_detail: Recipe;
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
+  }
+  addIngredientsToShoppingList() {
+    // for (let ingredient of this.recipe_detail.ingredients) {
+    //   this.shoppingListService.addIngredient(ingredient);
+    // }
+    this.shoppingListService.addMultipleIngredients(this.recipe_detail.ingredients);
   }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,23 +1,25 @@
 import { Ingredient } from './../shared/ingredient.model';
 import { EventEmitter, Injectable } from '@angular/core';
 
-@Injectable()
-// {providedIn: 'root'}
+@Injectable({ providedIn: 'root' })
+//
 export class ShoppingListService {
   // Event emitter to inform component about the new ingredient adding
   ingredientChangedEvent = new EventEmitter<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient("Apple", 5),
-    new Ingredient("Apple", 5),
-    new Ingredient("Apple", 5),
-    new Ingredient("Apple", 5),
-  ];
+    new Ingredient("Salt", 5),
+  ]
   constructor() { }
   getIngredients() {
     return this.ingredients.slice();
   }
   addIngredient(newIngredient: Ingredient) {
     this.ingredients.push(newIngredient);
+    this.ingredientChangedEvent.emit(this.ingredients.slice());
+  }
+  addMultipleIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
     this.ingredientChangedEvent.emit(this.ingredients.slice());
   }
 }

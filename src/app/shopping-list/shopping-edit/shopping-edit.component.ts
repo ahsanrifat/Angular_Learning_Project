fee10510ccc6e1211dab2f15044cf0dc97a6cbff
ahsanrifat <frifat2013@gmail.com>
2021-01-04 21:26:50 +0600

    Transform ingredient add form with NgForm

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
 import { Ingredient } from './../../shared/ingredient.model';
 import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,8 +10,8 @@ import { ShoppingListService } from '../shopping-list.service';
 })
 export class ShoppingEditComponent implements OnInit {
   // child view reference to get the data from template
-  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
-  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
+  // @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
+  // @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
 
   // Emit event to pass name and amount to shopping-list (parent component)
   // @Output() ingredientAdded = new EventEmitter<Ingredient>();
@@ -18,12 +19,15 @@ export class ShoppingEditComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  onAddItem() {
+  onAddItem(form: NgForm) {
+    const data = (form.value)
     // declare something constant if you are not planning to change something
-    const newIngredient = new Ingredient(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value);
+    // const newIngredient = new Ingredient(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value);
     // this.ingredientAdded.emit(newIngredient);
     // Working Fine
     // console.log("New Ingredient shopping edit comp", newIngredient);
+
+    const newIngredient = new Ingredient(data['name'], data['amount']);
     this.shoppingListService.addIngredient(newIngredient);
   }
 }

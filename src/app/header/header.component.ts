f3e9444f62f3afcf899db2529583ca7ba8c7c7d1
ahsanrifat <frifat2013@gmail.com>
2021-01-09 18:19:17 +0600

    Solve a bug

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -7,13 +8,19 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   @Output() featureSelected = new EventEmitter<string>();
-  constructor() { }
+  constructor(private dataService: DataStorageService) { }
 
   ngOnInit(): void {
   }
   onSelect(feature: string) {
     this.featureSelected.emit(feature);
     // console.log(feature)
+  }
+  storeRecipes() {
+    this.dataService.storeRecipes();
+  }
+  fetchRecipes() {
+    this.dataService.fetchRecipes();
   }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,4 @@
+import { DataStorageService } from './../shared/data-storage.service';
 import { Ingredient } from '../shared/ingredient.model';
 import { EventEmitter, Injectable, Output } from '@angular/core';
 import { Recipe } from './recipes.model';
@@ -7,23 +8,29 @@ import { Recipe } from './recipes.model';
 export class RecipeService {
   observeRecipeArrayChangeEvent = new EventEmitter();
   // recipeSelectedEvent = new EventEmitter<Recipe>();
-  private recipes: Recipe[] = [
-    new Recipe("Nutty Sweet Delish", "test recipe",
-      "https://hips.hearstapps.com/delish/assets/17/39/1506456246-delish-healthy-chicken-casserole-1.jpg",
-      [new Ingredient("Nuts", 5), new Ingredient("Milk", 10), new Ingredient("Cream", 2)]),
-    new Recipe("Soya Pron Stick", "test recipe",
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-taco-lime-shrimp-still006-1527253576.jpeg",
-      [new Ingredient("Sauce", 5), new Ingredient("Pron", 5), new Ingredient("Oil", 5)]),
-    new Recipe("Brownie Blast", "test recipe",
-      "https://cdn.loveandlemons.com/wp-content/uploads/2020/03/baking-recipes-1.jpg",
-      [new Ingredient("Flour", 5), new Ingredient("Coco powder", 5), new Ingredient("Sugar", 5)]),
-    new Recipe("Fruit Salsa", "test recipe",
-      "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2019%2F11%2Fcranberry-salad.jpg",
-      [new Ingredient("Strawberry", 5), new Ingredient("Mango", 5), new Ingredient("Pine Apple", 5)]),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe("Nutty Sweet Delish", "test recipe",
+  //     "https://hips.hearstapps.com/delish/assets/17/39/1506456246-delish-healthy-chicken-casserole-1.jpg",
+  //     [new Ingredient("Nuts", 5), new Ingredient("Milk", 10), new Ingredient("Cream", 2)]),
+  //   new Recipe("Soya Pron Stick", "test recipe",
+  //     "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-taco-lime-shrimp-still006-1527253576.jpeg",
+  //     [new Ingredient("Sauce", 5), new Ingredient("Pron", 5), new Ingredient("Oil", 5)]),
+  //   new Recipe("Brownie Blast", "test recipe",
+  //     "https://cdn.loveandlemons.com/wp-content/uploads/2020/03/baking-recipes-1.jpg",
+  //     [new Ingredient("Flour", 5), new Ingredient("Coco powder", 5), new Ingredient("Sugar", 5)]),
+  //   new Recipe("Fruit Salsa", "test recipe",
+  //     "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2019%2F11%2Fcranberry-salad.jpg",
+  //     [new Ingredient("Strawberry", 5), new Ingredient("Mango", 5), new Ingredient("Pine Apple", 5)]),
+  // ];
+  private recipes: Recipe[] = [];
   constructor() { }
+  setRecipeArray(recipeArray: Recipe[]) {
+    this.recipes = recipeArray;
+    this.observeRecipeArrayChangeEvent.emit();
+  }
   getRecipes() {
     // slice return another object of the array so that main recipe array can not be modified from outside
+    // this.dataService.fetchRecipes();
     return this.recipes.slice();
   }
   getSingleRecipe(index: number) {

7f194b90eabc1892cc9e5db02e27461fc58f5d97
ahsanrifat <frifat2013@gmail.com>
2021-01-11 12:13:29 +0600

    Add switching functions

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
+  isLoginMode = true;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
   }
 
 }
